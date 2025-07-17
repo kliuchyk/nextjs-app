9b4b336136a5aa94233f836f24c76626d572ee93
Yevhen Kliuchyk <kliuchyk@gmail.com>
2025-07-17 16:31:47 +0200

    handle sigin/signout

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,15 @@
+"use client";
+
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function NavBar() {
+  const { status, data: session } = useSession();
+
+  if (status === "loading") {
+    return null;
+  }
+
   return (
     <nav className="bg-slate-200 flex p-5">
       <Link href="/" className="mr-5">
@@ -9,6 +18,19 @@ export default function NavBar() {
       <Link href="/users" className="mr-5">
         Users
       </Link>
+      {status === "authenticated" && (
+        <>
+          <div>{session.user?.name}</div>
+          <Link className="ml-3" href="/api/auth/signout">
+            Sign out
+          </Link>
+        </>
+      )}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin" className="mr-5">
+          Login
+        </Link>
+      )}
     </nav>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
+import { getServerSession } from "next-auth";
+import { AuthOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(AuthOptions);
+
   return (
     <main>
-      <h1>Hello World!</h1>
+      <h1>Hello {session ? session.user?.name : "World"}!</h1>
 
       <Link href="/users">{`<< USERS >>`}</Link>
       <ProductCard />
