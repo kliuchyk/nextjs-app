6c3d9192b1bb3d5a2bbd16d83334aaaf550e8081
Yevhen Kliuchyk <kliuchyk@gmail.com>
2025-07-23 10:30:43 +0200

    add welcome template and img

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import ProductCard from "./components/ProductCard";
+import Image from "next/image";
+import garden from "@/public/images/garden.jpg";
 import { getServerSession } from "next-auth";
 import { AuthOptions } from "./api/auth/[...nextauth]/route";
 
@@ -11,7 +12,17 @@ export default async function Home() {
       <h1>Hello {session ? session.user?.name : "World"}!</h1>
 
       <Link href="/users">{`<< USERS >>`}</Link>
-      <ProductCard />
+
+      <Image alt="garden-image" src={garden} />
+      <Image
+        alt="garden-image"
+        height="400"
+        width="400"
+        className="object-cover"
+        src="https://images.pexels.com/photos/9100862/pexels-photo-9100862.jpeg"
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+      />
+      {/* <ProductCard /> */}
     </main>
   );
 }
